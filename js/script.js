488998bbc98e1950565807e8782ac8d1884ce9a6
Aziz <baziz1275@gmail.com>
2024-11-05 01:01:00 +0600

    HW-2 Operations and Expressions

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var san1 = prompt (" Birinshi sandy zhazynyz:")
+var san2 = prompt (" Ekinshi sandy zhazynyz:")
+var san3 = prompt (" ushinshi sandy zhazynyz:")
+var a = (+ san1 +  + san2 +  +san3) / 3
+
+alert("Arifmetikalyq orta:" + a);
