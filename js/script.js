485d78b2a7e01a604e3ec5131602c1abfb922a42
Aziz <baziz1275@gmail.com>
2024-11-05 01:01:35 +0600

    Operations and Expressions hw-2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-var san1 = prompt (" Birinshi sandy zhazynyz:")
-var san2 = prompt (" Ekinshi sandy zhazynyz:")
-var san3 = prompt (" ushinshi sandy zhazynyz:")
-var a = (+ san1 +  + san2 +  +san3) / 3
+// var san1 = prompt (" Birinshi sandy zhazynyz:")
+// var san2 = prompt (" Ekinshi sandy zhazynyz:")
+// var san3 = prompt (" ushinshi sandy zhazynyz:")
+// var a = (+ san1 +  + san2 +  +san3) / 3
 
-alert("Arifmetikalyq orta:" + a);
+// alert("Arifmetikalyq orta:" + a);
+
+
+
+//Home work 2
+
+//1)
+let adam = 70  
+let isStudent = true
+alert((adam >= 65) || (!isStudent))
+
+//2)
+
+let zhas = prompt("Zhasynyz;")
+alert(( zhas >= 10) && (zhas <= 20))
+
+//3)
+
+let s = -10
+let d = 5
+alert((s < 0 ) && (d > 0) || (s > 0) && (d < 0))
+
+
+//Адамның жеңілдік алуға құқылы екенін тексеретін бағдарлама жазыңыз. 
+//Егер адамның жасы 65-тен үлкен немесе ол студент болса, онда ол жеңілдік алуға құқылы деп саналады.
+// Жасты және студент екенін көрсететін айнымалыларды пайдаланыңыз, 
+//егер адам жеңілдік алуға құқылы болса, true деп шығарыңыз, ал егер жоқ болса, false деп шығарыңыз.
+
+/*  Енгізілген санның 10 мен 20 арасында екенін тексеретін бағдарлама жазыңыз (қоса алғанда).
+ Егер сан осы аралықта болса, true деп шығарыңыз, әйтпесе false деп шығарыңыз.
+Санды prompt() коды арқылы сұрап алыңыз.
+
+3.  Екі санның біреуінің теріс екенін тексеретін бағдарлама жазыңыз.
+Егер екі санның біреуі теріс болса, true деп шығарыңыз,
+ ал егер екеуі де оң немесе екеуі де теріс болса, false деп шығарыңыз.
+ */
+ 
